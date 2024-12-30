74380e89f193ed965984a5c5ae9c90f426a2993e
Michael Moser <109764421+MichaelMoser25@users.noreply.github.com>
2024-12-30 18:20:11 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,80 +1,82 @@
-/*
-	Radius by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
-
 (function($) {
+    skel.breakpoints({
+        xlarge: '(max-width: 1680px)',
+        large:  '(max-width: 1280px)',
+        medium: '(max-width: 980px)',
+        small:  '(max-width: 736px)',
+        xsmall: '(max-width: 480px)'
+    });
 
-	skel.breakpoints({
-		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
-		small:	'(max-width: 736px)',
-		xsmall:	'(max-width: 480px)'
-	});
+    $(function() {
+        var $window = $(window),
+            $body = $('body'),
+            $header = $('#header'),
+            $footer = $('#footer');
 
-	$(function() {
+        // Disable animations/transitions until the page has loaded.
+        $body.addClass('is-loading');
 
-		var	$window = $(window),
-			$body = $('body'),
-			$header = $('#header'),
-			$footer = $('#footer');
+        $window.on('load', function() {
+            window.setTimeout(function() {
+                $body.removeClass('is-loading');
+            }, 100);
+        });
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+        // Fix: Placeholder polyfill.
+        $('form').placeholder();
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
+        // Prioritize "important" elements on medium.
+        skel.on('+medium -medium', function() {
+            $.prioritize(
+                '.important\\28 medium\\29',
+                skel.breakpoint('medium').active
+            );
+        });
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+        // Header.
+        $header.each(function() {
+            var t = jQuery(this);
+            var inner = t.find('.inner');
+            var button = t.find('.button');
 
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+            // Add slide-out animation for the button click
+            button.click(function(e) {
+                e.preventDefault();
+                
+                // First shrink the circle
+                inner.animate({
+                    width: '6em',
+                    height: '6em',
+                    padding: '2em'
+                }, 500);
 
-		// Header.
-			$header.each( function() {
+                // Hide the content
+                inner.find('.content').fadeOut(300);
 
-				var t 		= jQuery(this),
-					button 	= t.find('.button');
+                // Then slide up off the screen
+                $header.animate({
+                    top: '-100vh'
+                }, 800, function() {
+                    // After animation, hide the header
+                    $header.css('display', 'none');
+                    // Scroll to the first section
+                    $('html, body').animate({
+                        scrollTop: $('.about-me').offset().top
+                    }, 500);
+                });
+            });
+        });
 
-				button.click(function(e) {
+        // Footer.
+        $footer.each(function() {
+            var t = jQuery(this),
+                inner = t.find('.inner'),
+                button = t.find('.info');
 
-					t.toggleClass('hide');
-
-					if ( t.hasClass('preview') ) {
-						return true;
-					} else {
-						e.preventDefault();
-					}
-
-				});
-
-			});
-
-		// Footer.
-			$footer.each( function() {
-
-				var t 		= jQuery(this),
-					inner 	= t.find('.inner'),
-					button 	= t.find('.info');
-
-				button.click(function(e) {
-					t.toggleClass('show');
-					e.preventDefault();
-				});
-
-			});
-
-	});
-
+            button.click(function(e) {
+                t.toggleClass('show');
+                e.preventDefault();
+            });
+        });
+    });
 })(jQuery);
